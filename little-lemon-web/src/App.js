f7d60023740d85d04244498aef748aa2aed9f713
Carla Bernardino <carlasofb@gmail.com>
2023-09-12 16:32:29 +0100

    Semantic structure

--- little-lemon-web/src/App.js
+++ little-lemon-web/src/App.js
@@ -1,7 +1,19 @@
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
 import "./App.css";
 
 function App() {
-  return <div className="App">Homepage</div>;
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ little-lemon-web/src/components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header>
+      <img src="../../assets/Logo.svg" alt="Little Lemon logo" />
+    </header>
+  );
+}
